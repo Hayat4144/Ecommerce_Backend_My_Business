ae86b0b499ab0a6352a4b41ce28fa06c2bbc4baa
hayat4144 <ihayat855@gmail.com>
2022-11-14 18:18:39 +0530

    added url for product.

--- shop/urls.py
+++ shop/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('api/get_all_product', views.get_all_product),
     path('api/get_all_categories', views.get_all_categories),
     path('api/get_product_by_category/<str:name>/', views.get_product_by_category),
+    path('api/get_product/<uuid:id>/', views.get_product_By_id),
+    path('api/get_product/details/<uuid:id>/', views.get_product_details),
     # path('api/get_attribute', views.get_attribute),
     ]
