016aa5638fba706721dc29636a5914050b56503f
hayat4144 <ihayat855@gmail.com>
2022-11-16 11:19:54 +0530

    serailzer edit.

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,6 +1,6 @@
 
 from rest_framework import serializers
-from .models import Category, Product
+from .models import Category, Product ,Produt_item
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -21,3 +21,13 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model =Category
         fields = ('id','name')
+
+class ProductItemSerializer(serializers.ModelSerializer):  
+    size_name = serializers.CharField(source ='size_id')
+    colour_name = serializers.CharField(source ='colour_id')
+    material_name = serializers.CharField(source ='material_id')
+    product_name = serializers.CharField(source ='product_id')
+    class Meta:
+        model = Produt_item
+        fields = ('id','sku','price','stock','size_name','colour_name','material_name' ,'product_name')
+     
