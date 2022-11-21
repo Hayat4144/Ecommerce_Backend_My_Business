5f4b80bd11d7382752a4a9362ad0ce472ecfdd5a
hayat4144 <ihayat855@gmail.com>
2022-11-21 19:55:26 +0530

    views created for stock.

--- Stocks/views.py
+++ Stocks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
 
 # Create your views here.
+
+@api_view(["GET"])
+def get_stock(request):
+    return JsonResponse('djf',safe=False)
