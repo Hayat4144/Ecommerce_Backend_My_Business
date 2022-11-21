4850496dcfde789fb91b19e69de5ca34a7edeb33
hayat4144 <ihayat855@gmail.com>
2022-11-21 19:04:59 +0530

    cart and cartItem serializer is created.

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,33 +1,50 @@
 
 from rest_framework import serializers
-from .models import Category, Product ,Produt_item
+from .models import Category, Product, Produt_item, shopping_cart, shopping_cart_item
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    category_name= serializers.CharField(source='categories')
+    category_name = serializers.CharField(source='categories')
+
     class Meta:
-        model= Product
-        fields = ('id','name','descriptions','regular_price','image','category_name')
+        model = Product
+        fields = ('id', 'name', 'descriptions',
+                  'regular_price', 'image', 'category_name')
 
 
 class ProductSerializerByCategory(serializers.ModelSerializer):
     # category_name= serializers.CharField(source='categories')
     class Meta:
-        model= Product
-        fields = ('id','name','descriptions','regular_price','sales_price','image','available_colors')
+        model = Product
+        fields = ('id', 'name', 'descriptions', 'regular_price',
+                  'sales_price', 'image', 'available_colors')
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
-        model =Category
-        fields = ('id','name')
+        model = Category
+        fields = ('id', 'name')
 
-class ProductItemSerializer(serializers.ModelSerializer):  
-    size_name = serializers.CharField(source ='size_id')
-    colour_name = serializers.CharField(source ='colour_id')
-    material_name = serializers.CharField(source ='material_id')
-    product_name = serializers.CharField(source ='product_id')
+
+class ProductItemSerializer(serializers.ModelSerializer):
+    size_name = serializers.CharField(source='size_id')
+    colour_name = serializers.CharField(source='colour_id')
+    material_name = serializers.CharField(source='material_id')
+    product_name = serializers.CharField(source='product_id')
+
     class Meta:
         model = Produt_item
-        fields = ('id','sku','price','stock','size_name','colour_name','material_name' ,'product_name')
-     
+        fields = ('id', 'sku', 'price', 'stock', 'size_name',
+                  'colour_name', 'material_name', 'product_name')
+
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = shopping_cart
+        fields = "__all__"
+
+
+class CartItemSerialzer(serializers.ModelSerializer):
+    class Meta:
+        model = shopping_cart_item
+        fields = "__all__"
