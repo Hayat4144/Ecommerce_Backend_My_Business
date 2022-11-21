d636bce9b7cd1c93f98c326dad762f1897815a50
hayat4144 <ihayat855@gmail.com>
2022-11-21 19:03:53 +0530

    product cart added.

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.db import models
-from .models import Product ,Category,size,colour,material, Produt_item
+from .models import Product ,Category,size,colour,material, Produt_item ,shopping_cart,shopping_cart_item
 
 class CustomProductAmdin(admin.ModelAdmin):
     model = Product
@@ -30,12 +30,23 @@ class materialAdmin(admin.ModelAdmin):
 class Product_itemAdmin(admin.ModelAdmin):
     model = Produt_item 
     list_filter = ('price',)
-    list_display = ('product_id','size_id','colour_id','price')
+    list_display = ('id','product_id','size_id','colour_id','price')
 
-    
+class Shopping_cartAdmin(admin.ModelAdmin):
+    model = shopping_cart
+    list_display = ('id','user_id')
+
+
+class Shopping_Cart_itemAdmin(admin.ModelAdmin):
+    model= shopping_cart_item
+    list_display = ('cart','product_item','quantity')
+    list_filter = ('product_item','cart')
+
 admin.site.register(Produt_item,Product_itemAdmin)
 admin.site.register(Product,CustomProductAmdin)
 admin.site.register(Category,CustomeCategoryAdmin)
 admin.site.register(size,SizeAdmin)
 admin.site.register(colour,ColourAdmin)
 admin.site.register(material,materialAdmin)
+admin.site.register(shopping_cart,Shopping_cartAdmin)
+admin.site.register(shopping_cart_item,Shopping_Cart_itemAdmin)
