e974619bf69d144d944328803cdd0730a5ee75d8
hayat4144 <ihayat855@gmail.com>
2022-11-28 20:15:10 +0530

    whishlist lurl is completed.

--- Whishlist/urls.py
+++ Whishlist/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
-    path('api/user/add_to_whishlist',views.add_to_whishlist)
+    path('api/user/add_to_whishlist',views.add_to_whishlist),
+    path('api/user/get_all_whishlist',views.get_all_wishlistItem),
+    path('api/user/delete_whishlist',views.delete_wishlistItem),
 ]
