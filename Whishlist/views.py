b8d21c64d43f83ab5554a8cc1958f400b443b5e1
hayat4144 <ihayat855@gmail.com>
2022-11-28 20:15:46 +0530

    views for whishlist item is completed.

--- Whishlist/views.py
+++ Whishlist/views.py
@@ -1,4 +1,4 @@
-from .models import UserWhishlist,WhishlistItem
+from .models import UserWhishlist, WhishlistItem
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 import json
@@ -6,9 +6,10 @@ from Backend.DecodeToken import decode_token
 from account.models import User
 from django.db import IntegrityError
 from rest_framework import status
+from .serailzers import WhishlistItemSerialzer
 
 
-# add to whishlist 
+# add to whishlist
 def add_to_whishlist(request):
     if request.method == "POST":
         whishlist_data = json.loads(request.body)
@@ -19,8 +20,9 @@ def add_to_whishlist(request):
             id=UserId, is_active=True).values()[0]['id']
         if CheckUserValid is None and len(CheckUserValid) < 1:
             return JsonResponse({"error": "Sorry User can't find."})
-        
-        whishlist,created = UserWhishlist.objects.get_or_create(user_id=CheckUserValid)
+
+        whishlist, created = UserWhishlist.objects.get_or_create(
+            user_id=CheckUserValid)
         try:
             IsProductExist = WhishlistItem.objects.filter(
                 productItem_id=whishlist_data['product_item_id'])
@@ -33,6 +35,45 @@ def add_to_whishlist(request):
         except IntegrityError:
             pass
 
-        JsonResponse('htlkjdsf' ,safe=False)
+        JsonResponse('htlkjdsf', safe=False)
     else:
         return JsonResponse({"error": f"{request.method} method is not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+
+
+# get wishlistItem of specific user only
+def get_all_wishlistItem(request):
+    if request.method == 'GET':
+        if not request.headers.get('Authorization'):
+            return JsonResponse({"error": "You are unauthorized."}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            jwt_token = request.headers.get('Authorization')
+            UserId = decode_token(jwt_token)
+            Wishlist_data = WhishlistItem.objects.filter(
+                whishlist__user=UserId)
+            serializer = WhishlistItemSerialzer(
+                instance=Wishlist_data, many=True)
+            return JsonResponse(serializer.data, safe=False)
+    else:
+        return JsonResponse({"error": f"The {request.method} method is not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+# delete whishlistItem
+def delete_wishlistItem(request):
+    if request.method == 'DELETE':
+        if not request.headers.get('Authorization'):
+            return JsonResponse({"error": "You are unauthorized."}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            jwt_token = request.headers.get('Authorization')
+            WhishlistItemId = json.loads(request.body)
+            if WhishlistItem.objects.filter(id=WhishlistItemId['whishlist_id']).exists():
+                UserId = decode_token(jwt_token)
+                delete_cartItem = WhishlistItem.objects.filter(
+                    whishlist__user=UserId, id=WhishlistItemId['whishlist_id'])
+                delete_cartItem.delete()
+                return JsonResponse({"data": "The Whishlist item has been deleted."}, safe=False)
+            else:
+                return JsonResponse({"error": "The Whishlist doesn't exist."}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return JsonResponse({"error": f"The {request.method} method is not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
