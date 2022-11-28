31067708234873d4d47f4dced39a2e9b2d9086cd
hayat4144 <ihayat855@gmail.com>
2022-11-28 16:16:16 +0530

    cart app url added.

--- Cart/urls.py
+++ Cart/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path
+from .views import add_to_cart ,get_all_cart_item
 
+urlpatterns = [
+    path('api/user/add_to_cart',add_to_cart),
+    path('api/user/get_cart_item',get_all_cart_item)
+]
