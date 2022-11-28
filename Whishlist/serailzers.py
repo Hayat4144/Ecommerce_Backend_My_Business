bf740ae942fe4c9ff9dc06c16a791aa6067a2a2c
hayat4144 <ihayat855@gmail.com>
2022-11-28 20:14:51 +0530

    whishlistitem serialzer is created.

--- Whishlist/serailzers.py
+++ Whishlist/serailzers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import WhishlistItem
 
 
-class CartItemSerialzer(serializers.ModelSerializer):
+class WhishlistItemSerialzer(serializers.ModelSerializer):
     class Meta:
         model = WhishlistItem
         fields = "__all__"
